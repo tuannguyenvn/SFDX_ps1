eb831c24603ed90fdc59795b59da5e69d183e248
tuannguyenvn <tuannguyen.de@gmail.com>
2021-01-13 16:29:48 +0100

    add datatable-Component

--- force-app/main/default/lwc/leadList/leadList.js
+++ force-app/main/default/lwc/leadList/leadList.js
@@ -1,50 +1,45 @@
 
 import { LightningElement, track} from 'lwc';
 
+
+const _COLS = [
+    {
+        label: 'Name',
+        fieldName: 'Name',
+        type: 'text'
+    },
+    {
+        label: 'Title',
+        fieldName: 'Title',
+        type: 'text'
+    },
+    {
+        label: 'Company',
+        fieldName: 'Company',
+        type: 'text'
+    },
+    {
+        label: 'View',
+        type: 'button-icon',
+        initialWidth: 75,
+        typeAttributes: {
+            title: 'View Details',
+            alternativeText: 'View Details',
+            iconName: 'action:info'
+
+        }
+    }
+];
+
 export default class LeadList extends (LightningElement) {
     @track leads =[];
     @track searchTerm;
+    @track cols = _COLS;
 
     
     handleSearchTermChange(event) {
         this.searchTerm = event.target.value;
         const selectedEvent = new CustomEvent('newsearch', {detail: this.searchTerm});
         this.dispatchEvent(selectedEvent);
-    }
-
-   
-    leads = [
-        {
-            "Id": "LeadRef1",
-            "Name": "Bertha Boxer",
-            "Title": "Director of Vendor Relations",
-            "Company": "Farmers Coop. of Florida",
-            "Street": "321 Westcott Building",
-            "City": "Tallahassee",
-            "State": "FL",
-            "PostalCode": "32306"
-        },
-        {
-            "Id": "LeadRef2",
-            "Name": "Phyllis Cotton",
-            "Title": "CFO",
-            "Company": "Chamber of Commerce",
-            "Street": "300 E Park Ave",
-            "City": "Tallahassee",
-            "State": "FL",
-            "PostalCode": "32301"
-        },
-        {
-            "Id": "LeadRef3",
-            "Name": "Jeff Glimpse",
-            "Title": "SVP, Procurement",
-            "Company": "Tallahassee Taxes",
-            "Street": "1327 Colorado St",
-            "City": "Tallahassee",
-            "State": "FL",
-            "PostalCode": "32304"
-        }
-    ];
-    
-    
+    }    
 }
