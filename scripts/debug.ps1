cb4ccccca5d1b64810fc547307503b808fd33d5c
tuannguyenvn <tuannguyen.de@gmail.com>
2021-01-14 13:09:58 +0100

    debug-Script

---
+++ scripts/debug.ps1
@@ -0,0 +1,10 @@
+sfdx force:source:deploy -m ApexClass
+#Or SFDX: Deploy Source To Org
+
+#SFDX: Toggle Checkpoint
+#SFDX: Update Checkpoints in Org
+
+#SFDX: Turn On Apex Debug Log for Replay Debugger
+#SFDX:Run Apex Test
+#SFDX:Get Apex Debug Logs
+#SFDX:Lauch Apex Replay Debugger With Current File 
