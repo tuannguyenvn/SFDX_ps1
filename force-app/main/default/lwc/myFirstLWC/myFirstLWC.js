10d762e139fbc6e6a087b0493a29c04815341c18
tuannguyenvn <tuannguyen.de@gmail.com>
2021-01-23 20:51:30 +0100

    add LeadMap-Page

--- force-app/main/default/lwc/myFirstLWC/myFirstLWC.js
+++ force-app/main/default/lwc/myFirstLWC/myFirstLWC.js
@@ -2,8 +2,15 @@ import { LightningElement, track } from 'lwc';
 
 export default class MyFirstLWC extends LightningElement {
     @track searchTerm;
+    @track searchInput;
+    @track searchComplete = false;
 
     handleNewSearch(event) {
         this.searchTerm = event.taget.value; 
+    }
+
+    handleSearchComplete(event) {
+        this.searchInput = event.detail;
+        this.searchComplete = true;
     }
 }
