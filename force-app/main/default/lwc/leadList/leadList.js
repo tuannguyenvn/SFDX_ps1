38ef1e467c5090f7566ff2769f7d2a1a68926f86
tuannguyenvn <tuannguyen.de@gmail.com>
2021-01-24 09:01:17 +0100

    add Leadlist-Test, Fix "Api Version not passt", "Event-Name-Error"

--- force-app/main/default/lwc/leadList/leadList.js
+++ force-app/main/default/lwc/leadList/leadList.js
@@ -1,82 +1,83 @@
-
-import { LightningElement, track, wire} from 'lwc';
-import searchLeads from '@salesforce/apex/LeadSearchController.searchLeads';
-import { NavigationMixin } from 'lightning/navigation';
+import { LightningElement, track, wire } from "lwc";
+import searchLeads from "@salesforce/apex/LeadSearchController.searchLeads";
+import { NavigationMixin } from "lightning/navigation";
 
 const DELAY = 350;
 
 const _COLS = [
-    {
-        label: 'Name',
-        fieldName: 'Name',
-        type: 'text'
-    },
-    {
-        label: 'Title',
-        fieldName: 'Title',
-        type: 'text'
-    },
-    {
-        label: 'Company',
-        fieldName: 'Company',
-        type: 'text'
-    },
-    {
-        label: 'View',
-        type: 'button-icon',
-        initialWidth: 75,
-        typeAttributes: {
-            title: 'View Details',
-            alternativeText: 'View Details',
-            iconName: 'action:info'
-
-        }
+  {
+    label: "Name",
+    fieldName: "Name",
+    type: "text"
+  },
+  {
+    label: "Title",
+    fieldName: "Title",
+    type: "text"
+  },
+  {
+    label: "Company",
+    fieldName: "Company",
+    type: "text"
+  },
+  {
+    label: "View",
+    type: "button-icon",
+    initialWidth: 75,
+    typeAttributes: {
+      title: "View Details",
+      alternativeText: "View Details",
+      iconName: "action:info"
     }
+  }
 ];
 
 export default class LeadList extends NavigationMixin(LightningElement) {
-    @track leads =[];
-    @track searchTerm;
-    @track cols = _COLS;
-    @track error;
+  @track leads = [];
+  @track searchTerm;
+  @track cols = _COLS;
+  @track error;
 
-    
-    handleSearchTermChange(event) {
-        this.searchTerm = event.target.value;
- 
-        if (this.leads) {
-            var selectedEvent = new CustomEvent('newsearch', {detail: this.searchTerm});
-            window.clearTimeout(this.delayTimeout);
-            this.delayTimeout = setTimeout(() => {
-                this.dispatchEvent(selectedEvent);
-            }, DELAY);           
-        }  
-    }    
+  handleSearchTermChange(event) {
+    this.searchTerm = event.target.value;
 
-    @wire(searchLeads, {
-        searchTerm: '$searchTerm'
-    })
-    loadLeads({ error, data }) {
-        if (data) {
-            this.leads = data;
-            var selectedEvent = new CustomEvent('searchcomplete', {detail: this.searchTerm});
-            this.dispatchEvent(new CustomEvent(selectedEvent));
-            this.error = undefined;
-        } else if (error) {
-            this.error = error;
-            this.leads = undefined;
-        }
+    if (this.leads) {
+      var selectedEvent = new CustomEvent("newsearch", {
+        detail: this.searchTerm
+      });
+      window.clearTimeout(this.delayTimeout);
+      this.delayTimeout = setTimeout(() => {
+        this.dispatchEvent(selectedEvent);
+      }, DELAY);
     }
+  }
 
-    handleRowAction(event) {
-        const row = event.detail.row;
-        this.record = row;
-        this[NavigationMixin.Navigate]({
-            type: 'standard__recordPage',
-            attributes: {
-                recordId: row.Id,
-                actionName: 'view',
-            },
-        });
+  @wire(searchLeads, {
+    searchTerm: "$searchTerm"
+  })
+  loadLeads({ error, data }) {
+    if (data) {
+      this.leads = data;
+      var selectedEvent = new CustomEvent("searchcomplete", {
+        detail: this.searchTerm
+      });
+      this.dispatchEvent(selectedEvent);
+      this.error = undefined;
+    } else if (error) {
+      this.error = error;
+      this.leads = undefined;
     }
+  }
+
+  handleRowAction(event) {
+    const row = event.detail.row;
+    this.record = row;
+    this[NavigationMixin.Navigate]({
+      type: "standard__recordPage",
+      attributes: {
+        recordId: row.Id,
+        actionName: "view"
+      }
+    });
+  }
 }
